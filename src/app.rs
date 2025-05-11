ab6226286c7047302f2972ca27500c1974d7a6be
Samuel Craig Carey <sam@samcarey.com>
2025-05-11 18:45:47 -0500

    use tailwind v3

--- src/app.rs
+++ src/app.rs
@@ -11,8 +11,8 @@ use leptos_router::{
 pub fn App() -> impl IntoView {
     provide_meta_context();
     view! {
-        <Stylesheet id="leptos" href="/style/tailwind.css"/>
-        <Link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✋</text></svg>"/>
+        <Stylesheet id="leptos" href="/style/output.css"/>
+        // <Link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✋</text></svg>"/>
         <Router>
             <main>
                 <Routes fallback=|| "Page not found.">
