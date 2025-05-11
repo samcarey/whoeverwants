8c0535946ddf0de8ee3d009c64cda950f120780c
Samuel Craig Carey <sam@samcarey.com>
2025-05-11 18:29:40 -0500

    fix css path

--- src/app.rs
+++ src/app.rs
@@ -11,7 +11,7 @@ use leptos_router::{
 pub fn App() -> impl IntoView {
     provide_meta_context();
     view! {
-        <Stylesheet id="leptos" href="/style/output.css"/>
+        <Stylesheet id="leptos" href="/style/tailwind.css"/>
         <Link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✋</text></svg>"/>
         <Router>
             <Routes fallback=|| "Page not found.">
